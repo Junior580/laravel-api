397ca6092683f4a29271d06e413c4e3969be6754
Junior <junior_silvapereira@hotmail.com>
2025-09-20 11:25:59 -0300

    views

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
+Route::get('/', function () {
+    return view('users.create');
+});
 
 Route::resource('users', UserController::class);
