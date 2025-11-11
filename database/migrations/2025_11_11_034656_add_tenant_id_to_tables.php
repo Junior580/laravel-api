e774df81eb2344bf8970770cb31cfc1ea6dab334
Júnior Pereira <junior@junior.localdomain>
2025-11-11 01:17:10 -0300

    feat: migraton to add tenant_id

---
+++ database/migrations/2025_11_11_034656_add_tenant_id_to_tables.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public const tables = ['products', 'services', 'categories', 'images', 'users'];
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        foreach (self::tables as $table) {
+            Schema::table($table, function (Blueprint $table) {
+                $table->foreignId('tenant_id')
+                    ->nullable()
+                    ->constrained('tenants');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        foreach (self::tables as $table) {
+            Schema::table($table, function (Blueprint $table) {
+                $table->dropForeign(['tenant_id']);
+                $table->dropColumn('tenant_id');
+            });
+        }
+    }
+};
